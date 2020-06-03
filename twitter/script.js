ccce4e642fafb97a76f4e5eeb0c5935cd5851408
Masakazu Yanai <masakazu-yanai@users.noreply.github.com>
2020-06-04 05:31:14 +0900

    1.0.1
    In the 'Notification' page, click and open url.

--- twitter/script.js
+++ twitter/script.js
@@ -1,5 +1,5 @@
 {
-	const fncCss = (sel, css) => {
+	const setCss = (sel, css) => {
 		document.querySelectorAll(sel).forEach(ele => {
 			if (ele.style.cssText !== '') { return }	
 			ele.style.cssText = css;
@@ -10,18 +10,40 @@
 		const url = '' + location.href;
 
 		if (url.match(RegExp('^https://twitter.com/notifications'))) {
-			fncCss(
+			// Change View.
+			setCss(
 				'.css-1dbjc4n.r-my5ep6.r-qklmqi.r-1adg3ll .r-qvutc0',
 				'color: #000 !important'
 			);
-			fncCss(
+			setCss(
 				'.css-1dbjc4n.r-my5ep6.r-qklmqi.r-1adg3ll .r-1s2bzr4',
 				'border: solid 1px #ccc !important;' +
 				'border-radius: 0.5em !important;' +
 				'padding: 0.75em 1em !important;'
 			);
+		} else
+		if (url.match(RegExp('^https://twitter.com/i/timeline'))) {
+			// Click TL item. -> Open new tab.
+			const ele = document.querySelector('time');
+			try {
+				ele.parentNode;
+			} catch(e) { return }
+			const eleP = ele.parentNode;
+			const url = 'https://twitter.com' + eleP.getAttribute('href');
+			history.back();
+
+			// Window Open
+			setTimeout(() => {
+				// Not Active (Background)
+				//const a = document.createElement('a');
+				//a.setAttribute('href', url);
+				//a.dispatchEvent(new MouseEvent('click', {ctrlKey: true}));
+
+				// Active
+				window.open(url, '_blank');
+			}, 500);
 		}
-	}, 500);
+	}, 200);
 }
 
 
