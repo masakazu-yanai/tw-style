87345d62689c5436477830a62adb5133ed0f2dcb
Masakazu Yanai <masakazu-yanai@users.noreply.github.com>
2020-06-05 05:26:09 +0900

    1.0.2

--- twitter/script.js
+++ twitter/script.js
@@ -1,4 +1,5 @@
 {
+	// Set Css
 	const setCss = (sel, css) => {
 		document.querySelectorAll(sel).forEach(ele => {
 			if (ele.style.cssText !== '') { return }	
@@ -6,21 +7,64 @@
 		});
 	};
 
+	// Check is CtrlKey
+	let isCtrlKey = false;
+
+	// Loop
 	setInterval(() => {
 		const url = '' + location.href;
 
 		if (url.match(RegExp('^https://twitter.com/notifications'))) {
-			// Change View.
+			// Change View
 			setCss(
 				'.css-1dbjc4n.r-my5ep6.r-qklmqi.r-1adg3ll .r-qvutc0',
 				'color: #000 !important'
 			);
 			setCss(
-				'.css-1dbjc4n.r-my5ep6.r-qklmqi.r-1adg3ll .r-1s2bzr4',
+				'.css-1dbjc4n.r-my5ep6.r-qklmqi.r-1adg3ll div[dir=ltr] ~ .css-1dbjc4n.r-1s2bzr4',
 				'border: solid 1px #ccc !important;' +
 				'border-radius: 0.5em !important;' +
 				'padding: 0.75em 1em !important;'
 			);
+
+			// Ctrl a clicking with CtrlKey
+			document.querySelectorAll('section[role=region] article')
+			.forEach((ele, i) => {
+				// Once
+				if (ele.isExClck) { return }
+				ele.isExClck = true;
+
+				// If Ctrl + Click -> Click
+				let lock = false;
+				ele.addEventListener('click', e => {
+					//console.log(i, e)
+					if (e.ctrlKey && !e.fromATag) {
+						if (!lock) {
+							e.preventDefault();
+							isCtrlKey = true;
+							lock = true;
+							let e2 = document.createEvent('Events');
+							e2.initEvent('click', true, false);
+							ele.dispatchEvent(e2);
+						}
+						return false;
+					}
+					lock = false;
+				})
+			});
+
+			// Prevent From Atag (for: Ctrl a clicking with CtrlKey)
+			document.querySelectorAll('section[role=region] article a')
+			.forEach((ele, i) => {
+				// Once
+				if (ele.isExClck) { return }
+				ele.isExClck = true;
+
+				ele.parentNode.addEventListener('click', e => {
+					e.fromATag = true;
+				});
+			});
+
 		} else
 		if (url.match(RegExp('^https://twitter.com/i/timeline'))) {
 			// Click TL item. -> Open new tab.
@@ -34,14 +78,17 @@
 
 			// Window Open
 			setTimeout(() => {
-				// Not Active (Background)
-				//const a = document.createElement('a');
-				//a.setAttribute('href', url);
-				//a.dispatchEvent(new MouseEvent('click', {ctrlKey: true}));
-
-				// Active
-				window.open(url, '_blank');
-			}, 500);
+				if (isCtrlKey) {
+					// Not Active (Background) - Click + Alt Key
+					const a = document.createElement('a');
+					a.setAttribute('href', url);
+					a.dispatchEvent(new MouseEvent('click', {ctrlKey: true}));
+				} else {
+					// Active - Click
+					window.open(url, '_blank');
+				}
+				isCtrlKey = false;
+			}, 200);
 		}
 	}, 200);
 }
